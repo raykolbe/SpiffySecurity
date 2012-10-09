53a31461307eebb0166fbd62637c4f4ac0958a63
Raymond Kolbe <rkolbe@gmail.com>
2012-10-09 15:43:31 -0300

    Update config/module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -5,9 +5,6 @@ return array(
         // see SpiffySecurity\Service\SecurityOptions for options
     ),
     'view_manager' => array(
-        'helper_map' => array(
-            'isGranted' => 'SpiffySecurity\View\Helper\IsGranted',
-        ),
         'template_path_stack' => array(__DIR__ . '/../view'),
     )
 );
